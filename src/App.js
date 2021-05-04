8d8107484c254c426a39ac5a750a1e45b173f11f
Troy Dorman <troydorman@gmail.com>
2021-05-04 00:16:55 -0600

    added basic appbar and implemented usestyles/makestyles for css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { CssBaseline, AppBar, Toolbar, Typography } from '@material-ui/core'
+import { Movie, Theaters } from '@material-ui/icons'
+import useStyles from './AppStyles'
+
 
 const App = () => {
+
+    const classes = useStyles()
+
     return (
-        <h1>OMDB SEARCH</h1>
+        <>
+        <CssBaseline />
+      <AppBar position="relative" className={classes.appBar}>
+        <Toolbar>
+          <Movie className={classes.iconLeft} />
+          
+          <Typography variant="h1"> movie of the year </Typography>
+
+          <Movie className={classes.iconRight} />
+        </Toolbar>
+      </AppBar>
+      </>
     )
 }
 
---
+++ src/AppStyles.js
@@ -0,0 +1,33 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+
+export default makeStyles((theme) => ({
+    
+    
+
+    iconLeft: {
+        marginRight: '20px',
+        fontSize: "55px"
+        
+
+    },
+    iconRight: {
+        marginLeft: '20px',
+        fontSize: "55px"
+        
+
+    },
+    appBar: {
+        background: '#2E3B55',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+       
+    
+    }
+    
+        
+
+}
+))
